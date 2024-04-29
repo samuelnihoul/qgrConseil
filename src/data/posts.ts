2d0578f6f5bfac5b6a0996aa577f73a7d8fb0c77
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2024-04-29 20:14:28 +0200

    linkedin

---
+++ src/data/posts.ts
@@ -0,0 +1,8 @@
+export default [
+  '<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7180096309951938560" height="1796" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>',
+  '<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7180096309951938560" height="1796" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>',
+  '<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7180096309951938560" height="1796" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>',
+  '<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7180096309951938560" height="1796" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>',
+  '<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7180096309951938560" height="1796" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>',
+  '<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7180096309951938560" height="1796" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>'
+]
--- src/pages/portfolio-single-column2.js
+++ src/pages/portfolio-single-column2.js
@@ -1,4 +1,5 @@
 import React from "react";
+import posts from '../data/posts.ts'
 import MainLayout from "../components/layout/MainLayout";
 import data from "../data/portfolio.json";
 import Link from "next/link";
@@ -7,52 +8,13 @@ const PortfolioSingleColumnpage = () => {
     <MainLayout>
       <div className="portfolio-two-column-section pt-130 mb-130">
         <div className="container">
-          <div className="row g-md-4 gy-5">
-            <div className="col-md-12">
-              {data.slice(0,5).map((i) => {
-                return (
-                  <div key={i.id} className="eg-card4 two mb-60">
-                    <div className="row g-0">
-                      <div className="col-md-6">
-                        <div className="card-img mb-0">
-                          <img src={i.img} alt="" />
-                        </div>
-                      </div>
-                      <div className="col-md-6">
-                        <div className="card-content">
-                          <div className="title-and-btn">
-                            <Link legacyBehavior href="/portfolio-single-column"><a>{i.tag}</a></Link>
-                            <h4>
-                              <Link legacyBehavior href="/portfolio-details"><a>{i.title}</a></Link>
-                            </h4>
-                          </div>
-                          <div className="card-btn">
-                            <Link legacyBehavior href="/portfolio-details">
-                              <a className="explore-btn two btn-hover">
-                                Explore More
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width={12}
-                                  height={12}
-                                  viewBox="0 0 12 12"
-                                >
-                                  <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M10.1865 1.06237L0 11.2484L0.751627 12L10.9376 1.81347V8.85645H12V0H3.14355V1.06237H10.1865Z"
-                                  />
-                                </svg>
-                                <span />
-                              </a>
-                            </Link>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
+          <div className="linkedin">
+            {
+              posts.map(
+                (p) => {
+                  return (<div dangerouslySetInnerHTML={{ __html: p }}></div>)
+                })
+            }
           </div>
           <div className="row">
             <div className="col-lg-12">
@@ -92,7 +54,7 @@ const PortfolioSingleColumnpage = () => {
           </div>
         </div>
       </div>
-    </MainLayout>
+    </MainLayout >
   );
 };
 
