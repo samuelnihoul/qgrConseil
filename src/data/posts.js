5889f7f121d7543c1f698117ebf4751dcdd088d2
samuelnihoul <69475809+samuelnihoul@users.noreply.github.com>
2024-06-27 19:22:01 +0200

    Update posts.js

--- src/data/posts.js
+++ src/data/posts.js
@@ -3,5 +3,5 @@ export default [
 	'https://www.linkedin.com/embed/feed/update/urn:li:activity:7185549483718385664?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7185549483718385664%29',//vendredi j'ai 
 	'https://www.linkedin.com/embed/feed/update/urn:li:activity:7180478747936993280?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7180478747936993280%29',//le biais du survivant
 	'https://www.linkedin.com/embed/feed/update/urn:li:activity:7178317766586720259?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7178317766586720259%29',//ah la culture
-	'https://www.linkedin.com/embed/feed/update/urn:li:activity:7212012277024092160'//Jancovici
+	
 ]
